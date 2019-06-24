25adc4874f4ea19eb3e6af4cfed95e503067ec3a
ls <479825880@qq.com>
2019-06-24 10:28:15 +0800

    leetcode24 两两交换链表中的节点

---
+++ src/com/link/Leetcode24.java
@@ -0,0 +1,63 @@
+package com.link;
+
+/**
+ * 两两交换链表中的节点
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * <p>
+ * @author liushuang 2019/6/24
+ */
+public class Leetcode24 {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        System.out.println(listNode1);
+        System.out.println(swapPairs(listNode1));
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        //终止条件：链表只剩一个节点或者没节点了，没得交换了。返回的是已经处理好的链表
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //一共三个节点:head, next, swapPairs(next.next)
+        //下面的任务便是交换这3个节点中的前两个节点
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        //返回给上一级的是当前已经完成交换后，即处理好了的链表部分
+        return next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
